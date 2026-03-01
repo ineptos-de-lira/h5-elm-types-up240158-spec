80b84e3e37cb58915e1f85b12639163140b5a728
becapeca <up240158@alumnos.upa.edu.mx>
2026-02-28 20:22:14 -0600

    Ex5 helper

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,55 @@
 module Helper exposing (..)
+
+import Html
+import Html.Attributes
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map transformador grades
+
+
+transformador : Float -> GradeStatus
+transformador calificacion =
+    if calificacion >= 7 then
+        Approved
+
+    else if calificacion >= 0 then
+        Failed
+
+    else
+        Pending
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction domain =
+    case domain of
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction uno =
+    List.map airplaneScheduleAction uno
